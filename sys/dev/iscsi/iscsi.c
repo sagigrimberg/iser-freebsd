sagigrimberg/iser-freebsd

commit bef3f8122ffacdb3e5c6b195003ac7fa2ad39e11

iscsi: Don't reconnect in case session is terminating
This fixes an iser crash.

Signed-off-by: Sagi Grimberg <sagig@mellanox.com>

sys/dev/iscsi/iscsi.c
@@ -769,6 +769,12 @@ iscsi_error_callback(struct icl_conn *ic)
 
 	is = CONN_SESSION(ic);
 
+	/*
+	 * In case we terminating, don't reconnect
+	 */
+	if (is->is_terminating)
+		return;
+
 	ISCSI_SESSION_WARN(is, "connection error; reconnecting");
 	iscsi_session_reconnect(is);
 }

commit 8550c314509361ec2a08c216c2b54da6ff7c8062

iser: Handle iscsi logout correctly
Don't call iscsi_error_callback in case we are already in
state CONN_TERMINATING as this indicates that a logout
procedure had started.

And don't block iscsi from reconnecting upon iscsi_error_callback
(see the fixes tag).

Fixes: bef3f81 "Don't reconnect in case session is terminating"
Signed-off-by: Sagi Grimberg <sagig@mellanox.com>
Signed-off-by: Max Gurtovoy <maxg@mellanox.com>

sys/dev/iscsi/iscsi.c
@@ -769,12 +769,6 @@ iscsi_error_callback(struct icl_conn *ic)
 
 	is = CONN_SESSION(ic);
 
-	/*
-	 * In case we terminating, don't reconnect
-	 */
-	if (is->is_terminating)
-		return;
-
 	ISCSI_SESSION_WARN(is, "connection error; reconnecting");
 	iscsi_session_reconnect(is);
 }

sys/dev/iscsi/iser_verbs.c
@@ -78,11 +78,17 @@ static void iser_handle_wc(struct ib_wc *wc)
 			ISER_ERR("Unknown wc opcode %d", wc->opcode);
 		}
 	} else {
-		if (wc->status != IB_WC_WR_FLUSH_ERR)
+		if (wc->status != IB_WC_WR_FLUSH_ERR) {
+			struct iser_conn *iser_conn;
+
+			iser_conn = container_of(ib_conn, struct iser_conn, ib_conn);
+			iser_conn->icl_conn.ic_error(&iser_conn->icl_conn);
+
 			ISER_ERR("wr id %lx status %d vend_err %x",
 				 wc->wr_id, wc->status, wc->vendor_err);
-		else
+		} else {
 			ISER_DBG("flush error: wr id %lx\n", wc->wr_id);
+		}
 
 		if (wc->wr_id == ISER_BEACON_WRID) {
 			/* all flush errors were consumed */
@@ -590,6 +596,10 @@ iser_conn_terminate(struct iser_conn *iser_conn)
 
 	ISER_INFO("iser_conn %p", iser_conn);
 
+	mtx_lock(&iser_conn->state_mutex);
+	iser_conn->state = ISER_CONN_TERMINATING;
+	mtx_unlock(&iser_conn->state_mutex);
+
 	if (ib_conn->qp == NULL) {
 		/* HOW can this be??? */
 		ISER_WARN("qp wasn't created");
@@ -733,12 +743,10 @@ iser_cleanup_handler(struct rdma_cm_id *cma_id, bool destroy)
 {
 	struct iser_conn *iser_conn = cma_id->context;
 
-	if (iser_conn) {
-		mtx_lock(&iser_conn->state_mutex);
-		iser_conn->state = ISER_CONN_TERMINATING;
-		mtx_unlock(&iser_conn->state_mutex);
+	mtx_lock(&iser_conn->state_mutex);
+	if (iser_conn->state != ISER_CONN_TERMINATING)
 		iser_conn->icl_conn.ic_error(&iser_conn->icl_conn);
-	}
+	mtx_unlock(&iser_conn->state_mutex);
 };
 
 static int